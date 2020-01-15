d712068ddd2e37b9359c96f784590c166d3fa127
David Crawford <DaveAldon@users.noreply.github.com>
2020-01-15 16:36:01 -0500

    Added some queue logic and beginning BFS

--- Main.js
+++ Main.js
@@ -2,6 +2,7 @@ function Peg() {
     this.isFilled = true;
     this.cord = [];
     this.neighbors = [];
+    this.availableMoves = [];
 }
 Peg.prototype.addNeighbor = function(neighbor) {
     this.neighbors.push(neighbor)
@@ -12,20 +13,29 @@ Peg.prototype.setCord = function(cord) {
 Peg.prototype.changePeg = function(state) {
     this.isFilled = state
 }
+Peg.prototype.availableMoves = function(move) {
+    this.availableMoves.push(move)
+}
 
 var board = [];
+var moveQueue = [];
+var startingPeg;
+var TotalEmptyPegs = 1;
+
 // Main game state
 function Main() {
-    size = 4
+    size = 2
     board = buildBoard(size)
     SetNeighbors()
 
     var ranX = Math.floor(Math.random() * size)
     var ranY = Math.floor(Math.random() * ranX)
-
+    ranX = 0
+    ranY = 0
     SetEmptyPeg(ranX, ranY)
 
-    // Logic for displaying the board
+    StartGame()
+        // Logic for displaying the board
     board.forEach(function(row) {
         var display = ""
         row.forEach(function(aPeg) {
@@ -34,9 +44,64 @@ function Main() {
     })
 }
 
+function StartGame() {
+    // Start with the empty peg's neighbors, they're the only ones that can make a first move
+    for (var y = 0; y < startingPeg.neighbors.length; y++) {
+        var startingNeighbors = startingPeg.neighbors[y]
+        for (var i = 0; i < startingNeighbors.neighbors.length; i++) {
+            var firstTry = startingNeighbors.neighbors[i]
+            if (firstTry.isFilled) {
+                moveQueue.push(firstTry)
+                break;
+            }
+        }
+        break;
+    }
+    console.log(moveQueue)
+
+    while (moveQueue.length > 0) {
+        var current = moveQueue.shift();
+        if (TotalEmptyPegs == 5) {
+            console.log("You win!")
+            break;
+        }
+        var edges = current.neighbors
+        for (let i = 0; i < edges.length; i++) {
+            console.log("Edges:")
+            console.log(edges[i]);
+            // infinite loop, need to determine how to make a move
+            //moveQueue.push(edges[i])
+        }
+    }
+}
+
 // Set beginning empty peg
 function SetEmptyPeg(emptyX, emptyY) {
-    board[emptyX][emptyY].changePeg(false)
+    startingPeg = board[emptyX][emptyY]
+    startingPeg.changePeg(false)
+}
+
+function GetNeighbors(x, y) {
+    return [
+        [x - 1, y - 1],
+        [x - 1, y],
+        [x, y - 1],
+        [x, y + 1],
+        [x + 1, y],
+        [x + 1, y + 1]
+    ];
+}
+
+function GetAvailableMoves(me, dirs) {
+    var availableMoves = []
+    dirs.forEach(function(dir) {
+        var xDif = me[0] - dir[0]
+        var yDif = me[1] - dir[1]
+        var dif = [xDif, yDif]
+        var checkNode = board[xDif][yDif]
+        if (checkNode != undefined) availableMoves.push(checkNode)
+    })
+    return availableMoves
 }
 
 function SetNeighbors() {
@@ -46,14 +111,7 @@ function SetNeighbors() {
             // Current peg
             var me = board[x][y]
 
-            var neighbors = [
-                [x - 1, y - 1],
-                [x - 1, y],
-                [x, y - 1],
-                [x, y + 1],
-                [x + 1, y],
-                [x + 1, y + 1]
-            ];
+            var neighbors = GetNeighbors(x, y)
 
             // Go through each neighbor and add it if it's a valid reference
             neighbors.forEach(function(cord) {
